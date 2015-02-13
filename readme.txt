55901c1615846cf6eb00e56ca32950c6135026b5
flyplugins <flyplugins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-13 15:02:48 +0000

    Update 1.1
    git-svn-id: https://plugins.svn.wordpress.org/s2member-addon-for-wp-courseware/trunk@1089124 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -2,14 +2,16 @@
 Contributors: flyplugins
 Donate link: http://flyplugins.com/donate
 Tags: fly plugins, s2member,wp courseware,wpcourseware,lms,lms for wordpress,learning management system,online ecourse,ecourse,flyplugins,wordpress learning management system
-Requires at least: 3.0
-Tested up to: 3.6.1
-Stable tag: 1.0
+Requires at least: 3.7
+Tested up to: 4.1
+Stable tag: 1.1
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
 This plugin adds integration between S2Member and WP Courseware which allows you to assign course(s) to membership levels for auto enrollment.
 
 == Description ==
-[Fly Plugins](http://flyplugins.com) presents [S2Member](http://flyplugins.com/s2member) for [WP Courseware](http://www.wpcourseware.com).
+[Fly Plugins](http://flyplugins.com) presents [S2Member](http://flyplugins.com/s2member) for [WP Courseware](http://wpcourseware.com).
 
 = Have you ever wanted to sell an online internet course or ecourse? =
 The S2Member Addon for WP Courseware will add full integration with WP Courseware. It will allow you to assign WP Courseware courses to a membership level. Once the course(s) are assigned to a membership level, upon purchase of a membership level and registration, a student will automatically be enrolled into the associated courses.
@@ -17,28 +19,31 @@ The S2Member Addon for WP Courseware will add full integration with WP Coursewar
 With this addon, you will be able to create a fully automated LMS system which you can sell online, or if you choose to run a free online course, you can do so with ease.
 
 = S2 Membership Plugin Integration with WP Courseware Plugin =
-[youtube http://www.youtube.com/watch?v=MHwAhWe7Xg0]
+[youtube http://www.youtube.com/watch?v=HC0IMrLZuLU]
 
 = Basic Configuration Steps =
 1. Create a course with WP Courseware and add module(s), unit(s), and quiz(zes)
 2. Add units to modules, and add quizzes to units with in modules
 3. Create a course outline page using [shortcode]
-4. Associate one or more WP Courseware courses with the membership level
-5. New users pay for the course through the shopping cart integration of your choice, the membership plugin assigns them WP user access and assigns them to the membership level for which they paid, and WP Courseware assigns them to the appropriate course(s) based on the membership level
+4. Create a membership level and set a price
+5. Associate one or more WP Courseware courses with the membership level
+6. New users pay for membership level, and WP Courseware assigns them to the appropriate course(s) based on the membership level
 
 = Check out Fly Plugins =
 For more info about Fly Plugins Free and Premium Plugins, check out the following links:
 
-* [WP Courseware LMS plugin for WordPress](http://flyplugins.com/wp-courseware-premium-wordpress-plugin/) - The best LMS online for WordPress.
-* [S3 Media Maestro](http://www.s3mediamaestro.com/) - The best HTML 5 secure media player for WordPress that works hand in hand with Amazon S3.
-* Other [Fly PluginsWordPress Plugins](http://flyplugins.com) by Fly Plugins.
-* Follow Fly Plugins on [Facebook](http://flyplugins.com/facebook) 
-* Check out the Fly Plugins [YouTube](http://www.youtube.com/flyplugins) channel.
-* See documentation on our [Codex](http://flyplugins.com/codex) site.
+* [WP Courseware Learning Management System plugin for WordPress](http://wpcourseware.com/) - The best LMS online for WordPress.
+* [S3 Media Maestro](http://s3mediamaestro.com) - The best HTML 5 secure media player for WordPress that works hand in hand with Amazon S3.
+* Other [Fly Plugins WordPress Plugins](http://flyplugins.com) by Fly Plugins.
+* Follow Fly Plugins on [Facebook](http://facebook.com/flyplugins) 
+* Check out the Fly Plugins [YouTube](http://www.youtube.com/flyplugins) YouTube channel.
+
+= Disclaimer =
+This is not an actual membership plugin. It is only the integration, or “middle-man” between WP Courseware and S2Member.
 
 == Installation ==
 
-1. Upload the `S2 Member for WP Courseware addon` folder into the `/wp-content/plugins/` directory
+1. Upload the `S2 Member addon for WP Courseware` folder into the `/wp-content/plugins/` directory
 1. Activate the plugin through the 'Plugins' menu in WordPress
 1. Configure the plugin by going into Training Courses-->S2Member, then associate courses with membership levels (this assumes you already have courses and membership levels created).
 
@@ -54,7 +59,7 @@ Yes!
 
 = Where can I get WP Courseware? =
 
-Click here to get the [Best WordPress LMS Plugin](http://www.wpcourseware.com).
+Click here to get the [Best WordPress LMS Plugin](http://wpcourseware.com).
 
 = Where can I get S2Member? =
 
@@ -67,6 +72,11 @@ Click here to get [S2Member](http://flyplugins.com/s2member).
 2. This is the actual configuration screen where you can select courses that will be associated with a particular membership level
 
 == Changelog ==
+
+= 1.1 =
+* New: Added function to retroactively assign new courses to existing membership levels that have already been purchased
+* Tweak: Added a database clean up function to rename the level IDs in the wpcw_member_level table
+* Tweak: Changed level ID naming scheme from s2member_x to s2member_level(x) to match role name
 
 = 1.0 =
 * Initial release
--- wp-courseware-s2-member.php
+++ wp-courseware-s2-member.php
@@ -1,40 +1,37 @@
 <?php
 /*
  * Plugin Name: WP Courseware - s2Member Add On
- * Version: 1.0
+ * Version: 1.1
  * Plugin URI: http://flyplugins.com
  * Description: The official extension for WP Courseware to add support for the s2Member membership plugin for WordPress.
  * Author: Fly Plugins
  * Author URI: http://flyplugins.com
  */
-/*
- Copyright 2013 Fly Plugins - Evolution Media Services, LLC
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
 
 // Main parent class
 include_once 'class_members.inc.php';
+
+// Clean up membership level IDs to match role name up activation
+function wpcw_s2_db_cleanup(){
+	global $wpdb;
+	$count_roles = $GLOBALS['WS_PLUGIN__']['s2member']['c']['levels'];
+		for ($i = 0; $i <= $count_roles; $i++){
+			$do_it = $wpdb->get_var( $wpdb->prepare( "UPDATE {$wpdb->prefix}wpcw_member_levels SET member_level_id = 's2member_level%d' WHERE member_level_id = 's2member_%d'",$i, $i) );
+		}
+	}
+
+	register_activation_hook( __FILE__, 'wpcw_s2_db_cleanup' );
 
 // Hook to load the class
 add_action('init', 'WPCW_Members_S2_init',1);
 
 /**
- * Initialise the membership plugin, only loaded if WP Courseware 
+ * Initialize the membership plugin, only loaded if WP Courseware 
  * exists and is loading correctly.
  */
 function WPCW_Members_S2_init()
 {
+
 	$item = new WPCW_Members_S2Member();
 	
 	// Check for WP Courseware
@@ -51,8 +48,8 @@ function WPCW_Members_S2_init()
 	
 	// Found the tool and WP Coursewar, attach.
 	$item->attachToTools();
-}
 
+}
 
 /**
  * Membership class that handles the specifics of the s2Member WordPress plugin and
@@ -69,13 +66,12 @@ class WPCW_Members_S2Member extends WPCW_Members
 	 */
 	function __construct()
 	{
-		// Initialise using the parent constructor 
+		// Initialize using the parent constructor 
 		parent::__construct(WPCW_Members_S2Member::EXTENSION_NAME, WPCW_Members_S2Member::EXTENSION_ID, WPCW_Members_S2Member::GLUE_VERSION);
 	}
 	
-	
 	/**
-	 * Get the membership levels for this specific membership plugin. (id => array (of details))
+	 * Get list of membership levels
 	 */
 	protected function getMembershipLevels()
 	{
@@ -94,9 +90,9 @@ class WPCW_Members_S2Member extends WPCW_Members
 			$levelItem = array();
 			$levelItem['name'] 	= $GLOBALS["WS_PLUGIN__"]["s2member"]["o"]['level' . $i . '_label'];
 			
-			// Using s2member as part of string because the levels are just numbers. Just minimises
+			// Using s2member as part of string because the levels are just numbers. Just minimizes
 			// any clashes by making the level ID a little more meaningful.
-			$levelItem['id'] 	= 's2member_' . $i;
+			$levelItem['id'] 	= 's2member_level' . $i;
 							
 			$levelDataStructured[$levelItem['id']] = $levelItem;
 		}
@@ -104,7 +100,6 @@ class WPCW_Members_S2Member extends WPCW_Members
 		return $levelDataStructured;
 	}
 
-	
 	/**
 	 * Function called to attach hooks for handling when a user is updated or created.
 	 */	
@@ -113,8 +108,41 @@ class WPCW_Members_S2Member extends WPCW_Members
 		// Update course access whenever the user role is change. Best that's possible with s2member 
 		add_action('set_user_role', array($this, 'handle_updateUserCourseAccess'), 10);
 	}
-	
 
+	
+	/**
+	 * Assign selected courses to members of a paticular level.
+	 * @param Level ID in which members will get courses enrollment adjusted.
+	 */
+	protected function retroactive_assignment($level_ID)
+    {
+    	global $wpdb;
+
+    	$page = new PageBuilder(false);
+
+    	// $levelID = substr($level_ID, strpos($level_ID, "_") + 1);
+		// $level_ID = 'S2member_level' . $levelID;
+
+		$results = get_users( array( 'role' => $level_ID ) );
+
+		if ($results){
+
+			// Get user's assigned products and enroll them into courses accordingly
+			foreach ($results as $result){
+				$id = $result->ID;
+				$s2member_access_level = 's2member_level' .get_user_field ("s2member_access_level", $id);	
+				parent::handle_courseSync($id, array($s2member_access_level));
+			}
+
+		$page->showMessage(__('All members were successfully retroactively enrolled into the selected courses.', 'wp_courseware'));
+            
+        return;
+
+		}else {
+            $page->showMessage(__('No existing customers found for the specified product.', 'wp_courseware'));
+        }
+    }
+	
 	/**
 	 * Function just for handling the membership callback, to interpret the parameters
 	 * for the class to take over.
@@ -123,12 +151,11 @@ class WPCW_Members_S2Member extends WPCW_Members
 	 */
 	public function handle_updateUserCourseAccess($id)
 	{		
-		$s2member_access_level = 's2member_' .get_user_field ("s2member_access_level", $id);		
+		$s2member_access_level = 's2member_level' .get_user_field ("s2member_access_level", $id);		
 						
 		// Over to the parent class to handle the sync of data.
 		parent::handle_courseSync($id, array($s2member_access_level));
 	}
-	
 	
 	/**
 	 * Detect presence of the membership plugin.
@@ -137,8 +164,6 @@ class WPCW_Members_S2Member extends WPCW_Members
 	{
 		return function_exists('ws_plugin__s2member_configure_options_and_their_defaults');
 	}
-	
-	
 	
 }
 
